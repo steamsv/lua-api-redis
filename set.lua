2a20c8dd364fe273b4be3c7ed3319e8ff64e49d6
steamsv <49923565+steamsv@users.noreply.github.com>
2021-11-11 16:24:16 +0800

    Update set.lua

--- set.lua
+++ set.lua
@@ -20,10 +20,27 @@ red:set_timeout(1000)
 --建立连接  
 local ip = "127.0.0.1"  
 local port = 6379
+local redis_auth = "";
 local ok, err = red:connect(ip, port)  
 if not ok then  
     ngx.say("connect to redis error : ", err)  
     return close_redis(red)  
+end
+client:set_timeout(redis_connection_timeout)
+
+-- 优化验证密码操作 代表连接在连接池使用的次数，如果为0代表未使用，不为0代表复用 在只有为0时才进行密码校验
+local connCount, err = client:get_reused_times()
+-- 新建连接，需要认证密码
+if  0 == connCount then
+    local ok, err = client:auth(redis_auth)
+    if not ok then
+        errlog("failed to auth: ", err)
+        return
+    end
+    --从连接池中获取连接，无需再次认证密码
+elseif err then
+    errlog("failed to get reused times: ", err)
+    return
 end
 local request_method = ngx.var.request_method
 local args = nil
